41982fed8634392489e740820b1e97f4eab91ef1
Kavishka <kavishkaprabath21@gmail.com>
2022-06-16 16:22:32 +0530

    create packages

---
+++ POS_System_BackEnd/src/servlet/CustomerServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("request ");
+    }
+}
+//http://localhost:8080/javaEEpos/customer
