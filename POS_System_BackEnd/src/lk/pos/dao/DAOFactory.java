10007afcec8ba78ad40d494f32f28d59f588ff3a
Kavishka <kavishkaprabath21@gmail.com>
2022-06-17 12:01:30 +0530

    add dao package

---
+++ POS_System_BackEnd/src/lk/pos/dao/DAOFactory.java
@@ -0,0 +1,32 @@
+package lk.pos.dao;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+    }
+
+    public static DAOFactory getDaoFactory() {
+        if (daoFactory == null) {
+            daoFactory = new DAOFactory();
+        }
+        return daoFactory;
+    }
+
+    public SuperDAO getDAO(DAOTypes types){
+
+        switch (types){
+            default:
+                return null;
+        }
+    }
+
+    public enum DAOTypes{
+    }
+}
---
+++ POS_System_BackEnd/src/lk/pos/dao/SuperDAO.java
@@ -0,0 +1,9 @@
+package lk.pos.dao;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+public class SuperDAO {
+}
