d50662609b95b6cef4462aff020465c403a7c933
Kavishka <kavishkaprabath21@gmail.com>
2022-06-18 23:22:38 +0530

    add doPost method for  ItemServelt

--- POS_System_BackEnd/src/lk/pos/controller/ItemServlet.java
+++ POS_System_BackEnd/src/lk/pos/controller/ItemServlet.java
@@ -3,6 +3,7 @@ package lk.pos.controller;
 import javafx.collections.ObservableList;
 import lk.pos.bo.BOFactory;
 import lk.pos.bo.custom.ItemBO;
+import lk.pos.dto.ItemDTO;
 
 import javax.annotation.Resource;
 import javax.json.*;
@@ -30,4 +31,57 @@ public class ItemServlet extends HttpServlet {
     DataSource dataSource;
 
     private final ItemBO itemBO = (ItemBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.ITEM);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("application/json");
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+
+        try {
+            Connection connection = dataSource.getConnection();
+            ItemDTO itemDTO = new ItemDTO(
+                    jsonObject.getString("itemCode"),
+                    jsonObject.getString("itemName"),
+                    Integer.parseInt(jsonObject.getString("itemQty")),
+                    Double.parseDouble(jsonObject.getString("itemPrice"))
+            );
+
+            System.out.println(itemDTO);
+
+            if (itemBO.addItem(connection, itemDTO)){
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+                objectBuilder.add("status", 200);
+                objectBuilder.add("message", "Successfully Added");
+                objectBuilder.add("data", "");
+                writer.print(objectBuilder.build());
+            }
+
+            connection.close();
+
+        } catch (SQLException e) {
+
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("status", 400);
+            objectBuilder.add("message", "Error");
+            objectBuilder.add("data", e.getLocalizedMessage());
+            writer.print(objectBuilder.build());
+            resp.setStatus(HttpServletResponse.SC_OK);
+            e.printStackTrace();
+
+        } catch (ClassNotFoundException e) {
+
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("status", 400);
+            objectBuilder.add("message", "Error");
+            objectBuilder.add("data", e.getLocalizedMessage());
+            writer.print(objectBuilder.build());
+            e.printStackTrace();
+        }
+
+    }
 }
