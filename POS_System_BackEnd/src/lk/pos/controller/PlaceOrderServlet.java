5e866f87751f6c4ce6b2c719100b2ef978a4e277
Kavishka <kavishkaprabath21@gmail.com>
2022-06-21 21:29:08 +0530

    add doGet method for PlaceOrderServlet

--- POS_System_BackEnd/src/lk/pos/controller/PlaceOrderServlet.java
+++ POS_System_BackEnd/src/lk/pos/controller/PlaceOrderServlet.java
@@ -3,6 +3,8 @@ package lk.pos.controller;
 import javafx.collections.ObservableList;
 import lk.pos.bo.BOFactory;
 import lk.pos.bo.custom.OrderBO;
+import lk.pos.dto.OrderDetailsDTO;
+import lk.pos.dto.OrdersDTO;
 
 import javax.annotation.Resource;
 import javax.json.*;
@@ -31,5 +33,87 @@ public class PlaceOrderServlet extends HttpServlet {
     DataSource dataSource;
 
     private final OrderBO orderBO = (OrderBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.ORDERS);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+
+            String option = req.getParameter("option");
+            String orderID = req.getParameter("orderId");
+            resp.setContentType("application/json");
+            Connection connection = dataSource.getConnection();
+            PrintWriter writer = resp.getWriter();
+
+            switch (option){
+
+                case "SEARCH":
+
+                    ArrayList<OrderDetailsDTO> orderDetails = orderBO.searchOrderDetails(orderID, connection);
+
+                    JsonArrayBuilder arrayBuilder1 = Json.createArrayBuilder();
+
+                    for (OrderDetailsDTO orderDetail : orderDetails) {
+                        JsonObjectBuilder ob = Json.createObjectBuilder();
+                        ob.add("oId",orderDetail.getoId());
+                        ob.add("iCode",orderDetail.getiCode());
+                        ob.add("qty",orderDetail.getoQty());
+                        ob.add("price",orderDetail.getPrice());
+                        ob.add("total",orderDetail.getTotal());
+
+                        arrayBuilder1.add(ob.build());
+                    }
+                    writer.write(String.valueOf(arrayBuilder1.build()));
+
+                    break;
+
+                case "GETID":
+
+                    JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+                    jsonObjectBuilder.add("orderId",orderBO.generateNewOrderId(connection));
+                    writer.print(jsonObjectBuilder.build());
+
+                    break;
+
+                case "GETALL":
+
+                    ObservableList<OrdersDTO> allOrders = orderBO.getAllOrders(connection);
+                    JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+                    for (OrdersDTO ordersDTO : allOrders){
+
+                        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                        objectBuilder.add("orderId", ordersDTO.getOrderId());
+                        objectBuilder.add("cid", ordersDTO.getcId());
+                        objectBuilder.add("orderDate", String.valueOf(ordersDTO.getOrderDate()));
+                        objectBuilder.add("total", ordersDTO.getTotal());
+                        objectBuilder.add("discount", ordersDTO.getDiscount());
+                        objectBuilder.add("subTotal", ordersDTO.getSubTotal());
+                        arrayBuilder.add(objectBuilder.build());
+
+                    }
+
+                    JsonObjectBuilder response = Json.createObjectBuilder();
+                    response.add("status", 200);
+                    response.add("message", "Done");
+                    response.add("data", arrayBuilder.build());
+                    writer.print(response.build());
+
+                    break;
+
+                case "COUNT":
+
+                    writer.print(orderBO.ordersCount(connection));
+
+                    break;
+            }
+
+            connection.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
