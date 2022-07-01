89b128be70bd84829ae2fe5b466203a352a55c81
Kavishka <kavishkaprabath21@gmail.com>
2022-07-01 21:09:23 +0530

    add LoadAllOrders Function to load order table

--- POS_System_FrontEnd/assets/script/OrderDetailsController.js
+++ POS_System_FrontEnd/assets/script/OrderDetailsController.js
@@ -2,4 +2,24 @@
  * @ author : Kavishka Prabath
  * @ since : 0.1.0
  **/
-    
+
+loadAllOrders();
+
+/* ------------------- Load All Orders to Order Table------------------- */
+function loadAllOrders(){
+    $("#orderTable").empty();
+    $.ajax({
+        url: "http://localhost:8080/pos/orders?option=GETALL",
+        method: "GET",
+        success: function (resp) {
+            for (const orders of resp.data) {
+
+                let row = `<tr><td>${orders.orderId}</td><td>${orders.cid}</td><td>${orders.orderDate}</td><td>
+                ${orders.total}</td><td>${orders.discount}</td><td>${orders.subTotal}</td></tr>`;
+                $("#orderTable").append(row);
+
+            }
+            //bindOrderDetailsClickEvent();
+        }
+    });
+}
