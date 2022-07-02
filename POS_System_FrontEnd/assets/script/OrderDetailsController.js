a855bc3b0c7159ffefc41848c9fc86fc5ecee2dc
Kavishka <kavishkaprabath21@gmail.com>
2022-07-03 00:32:17 +0530

    blindOrderDetailsClickEvent function add to OrderDetailsController

--- POS_System_FrontEnd/assets/script/OrderDetailsController.js
+++ POS_System_FrontEnd/assets/script/OrderDetailsController.js
@@ -19,6 +19,33 @@ function loadAllOrders(){
                 $("#orderTable").append(row);
 
             }
+            bindOrderDetailsClickEvent();
         }
     });
 }
+
+function bindOrderDetailsClickEvent(){
+    $("#orderTable > tr").click('click', function () {
+
+        tableRow = $(this);
+        let oid = $(this).children(":eq(0)").text();
+
+        $("#orderDetailTable").empty();
+        $.ajax({
+            url: "http://localhost:8080/pos/orders?option=SEARCH&orderId=" + oid,
+            method: "GET",
+            success: function (resp) {
+                for (const orders of resp) {
+
+                    let row = `<tr><td>${orders.oId}</td><td>${orders.iCode}</td><td>${orders.qty}</td><td>
+                    ${orders.price}</td><td>${orders.total}</td></tr>`;
+                    $("#orderDetailTable").append(row);
+
+                }
+            }
+
+        });
+    });
+}
+
+bindOrderDetailsClickEvent();
